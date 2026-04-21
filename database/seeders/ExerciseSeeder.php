5e1c7f53aa5693dc33a265a247c56c9b4e9a8887
Fer <fertadeusfp@gmail.com>
2026-04-21 09:21:24 +0200

    Añadidos más datos a las tablas
    Actualizado a su vez las tablas de ejercicios, y añadido un nuevo ejercicio

--- database/seeders/ExerciseSeeder.php
+++ database/seeders/ExerciseSeeder.php
@@ -44,7 +44,28 @@ class ExerciseSeeder extends Seeder
                         'genre' => 'RPG',
                         'release_year' => 2015,
                         'rating' => 9.8
-                    ]
+                    ],
+                    ['id'=>4,'title'=>'Red Dead Redemption 2','genre'=>'Action-Adventure','release_year'=>2018,'rating'=>9.7],
+                    ['id'=>5,'title'=>'God of War','genre'=>'Action','release_year'=>2018,'rating'=>9.6],
+                    ['id'=>6,'title'=>'Cyberpunk 2077','genre'=>'RPG','release_year'=>2020,'rating'=>7.5],
+                    ['id'=>7,'title'=>'Sekiro','genre'=>'Action','release_year'=>2019,'rating'=>9.4],
+                    ['id'=>8,'title'=>'Hades','genre'=>'Roguelike','release_year'=>2020,'rating'=>9.3],
+                    ['id'=>9,'title'=>'Celeste','genre'=>'Platformer','release_year'=>2018,'rating'=>9.2],
+                    ['id'=>10,'title'=>'Stardew Valley','genre'=>'Simulation','release_year'=>2016,'rating'=>9.5],
+                    ['id'=>11,'title'=>'Minecraft','genre'=>'Sandbox','release_year'=>2011,'rating'=>9.8],
+                    ['id'=>12,'title'=>'Terraria','genre'=>'Sandbox','release_year'=>2011,'rating'=>9.4],
+                    ['id'=>13,'title'=>'DOOM Eternal','genre'=>'Shooter','release_year'=>2020,'rating'=>9.1],
+                    ['id'=>14,'title'=>'Portal 2','genre'=>'Puzzle','release_year'=>2011,'rating'=>9.9],
+                    ['id'=>15,'title'=>'Half-Life 2','genre'=>'Shooter','release_year'=>2004,'rating'=>9.8],
+                    ['id'=>16,'title'=>'Among Us','genre'=>'Party','release_year'=>2018,'rating'=>8.5],
+                    ['id'=>17,'title'=>'Fortnite','genre'=>'Battle Royale','release_year'=>2017,'rating'=>8.0],
+                    ['id'=>18,'title'=>'League of Legends','genre'=>'MOBA','release_year'=>2009,'rating'=>8.7],
+                    ['id'=>19,'title'=>'Valorant','genre'=>'Shooter','release_year'=>2020,'rating'=>8.6],
+                    ['id'=>20,'title'=>'The Last of Us','genre'=>'Action-Adventure','release_year'=>2013,'rating'=>9.8],
+                    ['id'=>21,'title'=>'Bloodborne','genre'=>'RPG','release_year'=>2015,'rating'=>9.6],
+                    ['id'=>22,'title'=>'Ghost of Tsushima','genre'=>'Action','release_year'=>2020,'rating'=>9.3],
+                    ['id'=>23,'title'=>'Resident Evil 2','genre'=>'Horror','release_year'=>2019,'rating'=>9.2]
+
                 ]),
                 'course' => 'Introducción',
                 'subtitle' => 'Primer paso en el mundo de las consultas SELECT'
@@ -53,7 +74,7 @@ class ExerciseSeeder extends Seeder
             [
                 'id' => 2,
                 'title' => 'Selección por columna',
-                'description' => 'SQL también permite seleccionar columnas específicas. En entornos de trabajo podemos encontrarnos con tablas que contengan numerosas columnas. Seleccionando únicamente las columnas que nos interesa mostrar, podremos filtrar el resto. Para seleccionar las columnas específicas que necesitemos de una tabla escribiremos lo siguiente: <span class="font-monospace">SELECT nombre_de_columna1, nombre_de_columna2, ... FROM nombre_de_tabla;</span><br><br>Para completar el ejercicio, intenta mostrar las columnas <span class="font-monospace">title</span> y <span class="font-monospace">genre</span> de la tabla <span class="font-monospace">games</span>',
+                'description' => 'SQL también permite seleccionar columnas específicas. En entornos de trabajo podemos encontrarnos con tablas que contengan numerosas columnas. Seleccionando únicamente las columnas que nos interesa mostrar, podremos filtrar el resto. Para seleccionar las columnas específicas que necesitemos de una tabla escribiremos lo siguiente: <span class="font-monospace">SELECT nombre_de_columna1, nombre_de_columna2, ... FROM nombre_de_tabla;</span> Nótese que cuando seleccionamos más de una columna, hay que separar sus nombres entre sí con comas.<br><br>Para completar el ejercicio, intenta mostrar las columnas <span class="font-monospace">title</span> y <span class="font-monospace">genre</span> de la tabla <span class="font-monospace">games</span>',
                 'expected_sql' => 'SELECT title, genre FROM games;',
                 'expected_result' => json_encode([
                     [
@@ -67,7 +88,27 @@ class ExerciseSeeder extends Seeder
                     [
                         'title' => 'The Witcher 3',
                         'genre' => 'RPG'
-                    ]
+                    ],
+                    ['title'=>'Red Dead Redemption 2','genre'=>'Action-Adventure'],
+                    ['title'=>'God of War','genre'=>'Action'],
+                    ['title'=>'Cyberpunk 2077','genre'=>'RPG'],
+                    ['title'=>'Sekiro','genre'=>'Action'],
+                    ['title'=>'Hades','genre'=>'Roguelike'],
+                    ['title'=>'Celeste','genre'=>'Platformer'],
+                    ['title'=>'Stardew Valley','genre'=>'Simulation'],
+                    ['title'=>'Minecraft','genre'=>'Sandbox'],
+                    ['title'=>'Terraria','genre'=>'Sandbox'],
+                    ['title'=>'DOOM Eternal','genre'=>'Shooter'],
+                    ['title'=>'Portal 2','genre'=>'Puzzle'],
+                    ['title'=>'Half-Life 2','genre'=>'Shooter'],
+                    ['title'=>'Among Us','genre'=>'Party'],
+                    ['title'=>'Fortnite','genre'=>'Battle Royale'],
+                    ['title'=>'League of Legends','genre'=>'MOBA'],
+                    ['title'=>'Valorant','genre'=>'Shooter'],
+                    ['title'=>'The Last of Us','genre'=>'Action-Adventure'],
+                    ['title'=>'Bloodborne','genre'=>'RPG'],
+                    ['title'=>'Ghost of Tsushima','genre'=>'Action'],
+                    ['title'=>'Resident Evil 2','genre'=>'Horror']
                 ]),
                 'course' => 'Introducción',
                 'subtitle' => 'Seleccionar columnas específicas'
@@ -76,7 +117,7 @@ class ExerciseSeeder extends Seeder
             [
                 'id' => 3,
                 'title' => 'Cláusula WHERE',
-                'description' => 'La cláusula WHERE en SQL es utilizada para filtrar los datos en una consulta. Se utiliza para especificar condiciones que deben cumplirse para que una fila sea incluida en el resultado de la consulta. Por ejemplo, si queremos seleccionar las filas que tengan como título "Elden Ring", utilizaremos la siguiente sentencia: <span class="font-monospace">SELECT * FROM games WHERE title="Elden Ring";</span> ¡Recuerda que siempre puedes copiar y pegar la sentencia en el recuadro de abajo para comprobar los resultados!<br><br>Como ves, la sentencia sigue el siguiente esquema : <span class="font-monospace">SELECT nombre_de_columnas FROM nombre_de_tabla WHERE condición;</span> En este caso, "condición" hace referencia a un montón de posibilidades: puedes comparar números con <span class="font-monospace">=</span> , <span class="font-monospace">&lt;</span> o <span class="font-monospace">&gt;</span> , comparar strings... Iremos viendo dichas posibilidades una a una en los siguientes ejercicios.<br><br> De momento, trata de mostrar todos los datos de la tabla <span class="font-monospace">games</span> siempre y cuando el género sea <span class="font-monospace">RPG</span>',
+                'description' => 'La cláusula WHERE en SQL es utilizada para filtrar los datos en una consulta. Se utiliza para especificar condiciones que deben cumplirse para que una fila sea incluida en el resultado de la consulta. Por ejemplo, si queremos seleccionar las filas que tengan como título "Elden Ring", utilizaremos la siguiente sentencia: <span class="font-monospace">SELECT * FROM games WHERE title="Elden Ring";</span> ¡Recuerda que siempre puedes copiar y pegar la sentencia en el recuadro de abajo para comprobar los resultados!<br><br>Como ves, la sentencia sigue el siguiente esquema : <span class="font-monospace">SELECT nombre_de_columnas FROM nombre_de_tabla WHERE condición;</span> En este caso, "condición" hace referencia a un montón de posibilidades: puedes comparar números con <span class="font-monospace">=</span> , <span class="font-monospace">&lt;</span> o <span class="font-monospace">&gt;</span> , comparar strings... Iremos viendo dichas posibilidades una a una en los siguientes ejercicios.<br><br> De momento, trata de mostrar todos los datos de la tabla <span class="font-monospace">games</span> siempre y cuando el género sea <span class="font-monospace">RPG</span>. Cuando comparas cadenas de caracteres, la condición debe ir envuelta en comillas, como en el ejemplo anterior.',
                 'expected_sql' => 'SELECT * FROM games WHERE genre = "RPG";',
                 'expected_result' => json_encode([
                     [
@@ -92,11 +133,37 @@ class ExerciseSeeder extends Seeder
                         'genre' => 'RPG',
                         'release_year' => 2015,
                         'rating' => 9.8
-                    ]
+                    ],
+                    ['id'=>6,'title'=>'Cyberpunk 2077','genre'=>'RPG','release_year'=>2020,'rating'=>7.5],
+                    ['id'=>21,'title'=>'Bloodborne','genre'=>'RPG','release_year'=>2015,'rating'=>9.6]
+
                 ]),
                 'course' => 'Introducción',
                 'subtitle' => 'Introducción a la cláusula WHERE'
             ],
+        [
+                'id' => 4,
+                'title' => 'Cláusula WHERE 2',
+                'description' => 'Al utilizar la cláusula WHERE, podemos agregar más de una condición. Para ello usaremos <span class="font-monospace">AND</span>.<br><br> Por ejemplo, si queremos que la base de datos nos devuelva los datos de los juegos cuyo género sea <span class="font-monospace">Shooter</span> y cuya fecha de salida sea previa al 2010, utilizaremos <span class="font-monospace">SELECT * FROM games WHERE genre="Shooter" AND release_year &lt; 2010</span><br><br>En este ejercicio, intenta averiguar el <span class="font-monospace">title</span>, <span class="font-monospace">release_year</span> y <span class="font-monospace">rating</span> de los juegos que salieron antes del 2020 cuya nota sea superior a 9,5.',
+                'expected_sql' => 'SELECT title, release_year, rating FROM games WHERE release_year < 2020 AND rating > 9.5;',
+                'expected_result' => json_encode([
+                    [
+                        'title' => 'The Witcher 3',
+                        'release_year' => 2015,
+                        'rating' => 9.8
+                    ],
+                    ['title'=>'Red Dead Redemption 2','release_year'=>2018,'rating'=>9.7],
+                    ['title'=>'God of War','release_year'=>2018,'rating'=>9.6],
+                    ['title'=>'Minecraft','release_year'=>2011,'rating'=>9.8],
+                    ['title'=>'Portal 2','release_year'=>2011,'rating'=>9.9],
+                    ['title'=>'Half-Life 2','release_year'=>2004,'rating'=>9.8],
+                    ['title'=>'The Last of Us','release_year'=>2013,'rating'=>9.8],
+                    ['title'=>'Bloodborne','release_year'=>2015,'rating'=>9.6]
+
+                ]),
+                'course' => 'Introducción',
+                'subtitle' => 'Uso de AND'
+            ]
         ]);
     }
 }
--- database/seeders/GameDataSeeder.php
+++ database/seeders/GameDataSeeder.php
@@ -22,7 +22,7 @@ class GameDataSeeder extends Seeder
         DB::table('games')->truncate();
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-        // GAMES
+        // GAMES -> Los primeros están escritos así para que se vea clara su estructura, a partir de ahí, claridad de código
         DB::table('games')->insert([
             [
                 'title' => 'Elden Ring',
@@ -42,6 +42,26 @@ class GameDataSeeder extends Seeder
                 'release_year' => 2015,
                 'rating' => 9.8
             ],
+            ['title'=>'Red Dead Redemption 2','genre'=>'Action-Adventure','release_year'=>2018,'rating'=>9.7],
+            ['title'=>'God of War','genre'=>'Action','release_year'=>2018,'rating'=>9.6],
+            ['title'=>'Cyberpunk 2077','genre'=>'RPG','release_year'=>2020,'rating'=>7.5],
+            ['title'=>'Sekiro','genre'=>'Action','release_year'=>2019,'rating'=>9.4],
+            ['title'=>'Hades','genre'=>'Roguelike','release_year'=>2020,'rating'=>9.3],
+            ['title'=>'Celeste','genre'=>'Platformer','release_year'=>2018,'rating'=>9.2],
+            ['title'=>'Stardew Valley','genre'=>'Simulation','release_year'=>2016,'rating'=>9.5],
+            ['title'=>'Minecraft','genre'=>'Sandbox','release_year'=>2011,'rating'=>9.8],
+            ['title'=>'Terraria','genre'=>'Sandbox','release_year'=>2011,'rating'=>9.4],
+            ['title'=>'DOOM Eternal','genre'=>'Shooter','release_year'=>2020,'rating'=>9.1],
+            ['title'=>'Portal 2','genre'=>'Puzzle','release_year'=>2011,'rating'=>9.9],
+            ['title'=>'Half-Life 2','genre'=>'Shooter','release_year'=>2004,'rating'=>9.8],
+            ['title'=>'Among Us','genre'=>'Party','release_year'=>2018,'rating'=>8.5],
+            ['title'=>'Fortnite','genre'=>'Battle Royale','release_year'=>2017,'rating'=>8.0],
+            ['title'=>'League of Legends','genre'=>'MOBA','release_year'=>2009,'rating'=>8.7],
+            ['title'=>'Valorant','genre'=>'Shooter','release_year'=>2020,'rating'=>8.6],
+            ['title'=>'The Last of Us','genre'=>'Action-Adventure','release_year'=>2013,'rating'=>9.8],
+            ['title'=>'Bloodborne','genre'=>'RPG','release_year'=>2015,'rating'=>9.6],
+            ['title'=>'Ghost of Tsushima','genre'=>'Action','release_year'=>2020,'rating'=>9.3],
+            ['title'=>'Resident Evil 2','genre'=>'Horror','release_year'=>2019,'rating'=>9.2]
         ]);
 
         // PLATFORMS
@@ -63,6 +83,60 @@ class GameDataSeeder extends Seeder
             ['game_id' => 3, 'platform_id' => 1],
             ['game_id' => 3, 'platform_id' => 2],
             ['game_id' => 3, 'platform_id' => 3],
+
+            ['game_id'=>4,'platform_id'=>1],
+            ['game_id'=>4,'platform_id'=>2],
+            ['game_id'=>4,'platform_id'=>3],
+
+            ['game_id'=>5,'platform_id'=>2],
+
+            ['game_id'=>6,'platform_id'=>1],
+            ['game_id'=>6,'platform_id'=>2],
+
+            ['game_id'=>7,'platform_id'=>1],
+            ['game_id'=>7,'platform_id'=>2],
+
+            ['game_id'=>8,'platform_id'=>1],
+            ['game_id'=>8,'platform_id'=>4],
+
+            ['game_id'=>9,'platform_id'=>1],
+            ['game_id'=>9,'platform_id'=>4],
+
+            ['game_id'=>10,'platform_id'=>1],
+            ['game_id'=>10,'platform_id'=>4],
+
+            ['game_id'=>11,'platform_id'=>1],
+            ['game_id'=>11,'platform_id'=>2],
+            ['game_id'=>11,'platform_id'=>3],
+
+            ['game_id'=>12,'platform_id'=>1],
+
+            ['game_id'=>13,'platform_id'=>1],
+            ['game_id'=>13,'platform_id'=>3],
+
+            ['game_id'=>14,'platform_id'=>1],
+
+            ['game_id'=>15,'platform_id'=>1],
+
+            ['game_id'=>16,'platform_id'=>1],
+            ['game_id'=>16,'platform_id'=>4],
+
+            ['game_id'=>17,'platform_id'=>1],
+            ['game_id'=>17,'platform_id'=>2],
+            ['game_id'=>17,'platform_id'=>3],
+
+            ['game_id'=>18,'platform_id'=>1],
+
+            ['game_id'=>19,'platform_id'=>1],
+
+            ['game_id'=>20,'platform_id'=>2],
+
+            ['game_id'=>21,'platform_id'=>2],
+
+            ['game_id'=>22,'platform_id'=>2],
+
+            ['game_id'=>23,'platform_id'=>1],
+            ['game_id'=>23,'platform_id'=>2]
         ]);
 
         // DEVELOPERS
@@ -70,6 +144,22 @@ class GameDataSeeder extends Seeder
             ['name' => 'FromSoftware', 'country' => 'Japan'],
             ['name' => 'Team Cherry', 'country' => 'Australia'],
             ['name' => 'CD Projekt Red', 'country' => 'Poland'],
+            ['name'=>'Rockstar Games','country'=>'USA'],
+            ['name'=>'Santa Monica Studio','country'=>'USA'],
+            ['name'=>'CD Projekt Red','country'=>'Poland'],
+            ['name'=>'Supergiant Games','country'=>'USA'],
+            ['name'=>'Matt Makes Games','country'=>'Canada'],
+            ['name'=>'ConcernedApe','country'=>'USA'],
+            ['name'=>'Mojang','country'=>'Sweden'],
+            ['name'=>'Re-Logic','country'=>'USA'],
+            ['name'=>'id Software','country'=>'USA'],
+            ['name'=>'Valve','country'=>'USA'],
+            ['name'=>'Innersloth','country'=>'USA'],
+            ['name'=>'Epic Games','country'=>'USA'],
+            ['name'=>'Riot Games','country'=>'USA'],
+            ['name'=>'Naughty Dog','country'=>'USA'],
+            ['name'=>'Sucker Punch','country'=>'USA'],
+            ['name'=>'Capcom','country'=>'Japan']
         ]);
 
         // GAME_DEVELOPER (pivot)
@@ -77,6 +167,26 @@ class GameDataSeeder extends Seeder
             ['game_id' => 1, 'developer_id' => 1],
             ['game_id' => 2, 'developer_id' => 2],
             ['game_id' => 3, 'developer_id' => 3],
+            ['game_id'=>4,'developer_id'=>4],
+            ['game_id'=>5,'developer_id'=>5],
+            ['game_id'=>6,'developer_id'=>6],
+            ['game_id'=>7,'developer_id'=>1],
+            ['game_id'=>8,'developer_id'=>7],
+            ['game_id'=>9,'developer_id'=>8],
+            ['game_id'=>10,'developer_id'=>9],
+            ['game_id'=>11,'developer_id'=>10],
+            ['game_id'=>12,'developer_id'=>11],
+            ['game_id'=>13,'developer_id'=>12],
+            ['game_id'=>14,'developer_id'=>13],
+            ['game_id'=>15,'developer_id'=>13],
+            ['game_id'=>16,'developer_id'=>14],
+            ['game_id'=>17,'developer_id'=>15],
+            ['game_id'=>18,'developer_id'=>16],
+            ['game_id'=>19,'developer_id'=>16],
+            ['game_id'=>20,'developer_id'=>17],
+            ['game_id'=>21,'developer_id'=>1],
+            ['game_id'=>22,'developer_id'=>18],
+            ['game_id'=>23,'developer_id'=>19]
         ]);
     }
 }
