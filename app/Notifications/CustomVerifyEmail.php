d0b269b73d5330e931834b540dcac29aeae37e61
Fer <fertadeusfp@gmail.com>
2026-04-23 13:05:16 +0200

    Intento 11

--- app/Notifications/CustomVerifyEmail.php
+++ app/Notifications/CustomVerifyEmail.php
@@ -3,50 +3,21 @@
 namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
 
 class CustomVerifyEmail extends VerifyEmail
 {
-    use Queueable;
-
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct()
+    public function toMail($notifiable)
     {
-        //
-    }
-
-
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
-    {
-       $verificationUrl = $this->verificationUrl($notifiable);
+        $verificationUrl = $this->verificationUrl($notifiable);
 
         return (new MailMessage)
-            ->subject('Verifica tu cuenta en eSQLa')
+            ->subject('Verifica tu cuenta en SQL Trainer')
             ->greeting('Hola ' . $notifiable->name)
             ->line('Gracias por registrarte en eSQLa.')
             ->line('Haz clic en el botón para verificar tu cuenta.')
             ->action('Verificar email', $verificationUrl)
             ->line('Si no has creado esta cuenta, ignora este mensaje.')
-            ->salutation('Un saludo, eSQLa');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
-    {
-        return [
-            //
-        ];
+            ->salutation('Un saludo, SQL Trainer');
     }
 }
